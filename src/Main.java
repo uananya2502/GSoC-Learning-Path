d0cb41ed172fa3819a8f07ae5ada53acf4b29092
Ananya Upadhyay <uananya2502@gmail.com>
2025-12-17 12:01:22 +0530

    Added Tax calculator logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner; // Import the tool to take user input
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I am starting my GSoC journey!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter product price: ");
+        double price = input.nextDouble();
+
+        double taxRate = 0.18; // 18% tax
+        double totalTax = price * taxRate;
+        double finalPrice = price + totalTax;
+
+        System.out.println("--- Receipt ---");
+        System.out.println("Original Price: " + price);
+        System.out.println("Tax (18%): " + totalTax);
+        System.out.println("Final Amount: " + finalPrice);
     }
 }
